2853838c059205ac943edaafdfc74184a06e9934
rryan <ruthkryan@gmail.com>
2016-10-12 15:16:36 +0100

    javascript for section totals

--- src/main/webapp/resources/javascripts/balance-sheet.js
+++ src/main/webapp/resources/javascripts/balance-sheet.js
@@ -1,1 +1,17 @@
-$('#total-fixed-assets-current').val(+$('#intangible-assets-current').val() + +$('#tangible-assets-current').val() + +$('#investments-fixed-assets-current').val());
+function calculateBalanceSheetSectionTotal($total, $val1, $val2, $val3){
+
+$val1.add($val2).add($val3).on('change', function () 
+		{
+			$total.val(
+					+$val1.val() + +$val2.val() + +$val3.val()
+					);
+			}
+);
+}
+
+$(function() {
+		
+			calculateBalanceSheetSectionTotal($('#total-fixed-assets-current') ,$('#intangible-assets-current'),  $('#tangible-assets-current'),  $('#investments-fixed-assets-current'));
+			calculateBalanceSheetSectionTotal($('#total-fixed-assets-previous') ,$('#intangible-assets-previous'),  $('#tangible-assets-previous'),  $('#investments-fixed-assets-previous'));
+	}
+);
