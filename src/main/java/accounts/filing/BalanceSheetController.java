73ed91949c5ea56dba10493da107fa3a00893748
rryan <ruthkryan@gmail.com>
2016-10-18 12:45:07 +0100

    adding thymeleaf dependencies to balance sheet

--- src/main/java/accounts/filing/BalanceSheetController.java
+++ src/main/java/accounts/filing/BalanceSheetController.java
@@ -1,16 +1,21 @@
 package accounts.filing;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BalanceSheetController {
-	
-	
 
+	@Value({cdn.url}")
+	private String cdnUrl;
+	
 	@RequestMapping(value="/balanceSheet")
-	public String showBalanceSheetPage(){
+	public String showBalanceSheetPage(Model model){
+		model.addAttribute("cdUrl", cdnUrl);
 		return "balanceSheet";
+		
 	}
 	
 }
