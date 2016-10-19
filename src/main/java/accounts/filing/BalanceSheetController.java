972d07f3a4fe96e214c0a5898cab9c75845f1caf
flisowski <flisowski@companieshouse.gov.uk>
2016-10-19 11:01:12 +0100

    Thymeleaf working

--- src/main/java/accounts/filing/BalanceSheetController.java
+++ src/main/java/accounts/filing/BalanceSheetController.java
@@ -1,19 +1,22 @@
 package accounts.filing;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BalanceSheetController {
 
-	@Value({cdn.url}")
-	private String cdnUrl;
+public class BalanceSheetController {
+	
+
+	private String cdnUrl = "http://wswebdev2.orctel.internal:21502";
 	
 	@RequestMapping(value="/balanceSheet")
 	public String showBalanceSheetPage(Model model){
-		model.addAttribute("cdUrl", cdnUrl);
+				
+		model.addAttribute("cdnUrl", cdnUrl);
 		return "balanceSheet";
 		
 	}
