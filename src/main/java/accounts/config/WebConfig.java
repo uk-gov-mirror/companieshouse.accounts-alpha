09ad7d0070d9525bf575ed923772696c14595ba2
rryan <ruthkryan@gmail.com>
2016-10-20 10:57:22 +0100

    switching to java config for the servlet

---
+++ src/main/java/accounts/config/WebConfig.java
@@ -0,0 +1,48 @@
+package accounts.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring3.SpringTemplateEngine;
+import org.thymeleaf.spring3.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring3.view.ThymeleafViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({"accounts.filing"})
+public class WebConfig extends WebMvcConfigurerAdapter {
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+	@Bean
+	public SpringResourceTemplateResolver templateResolver(){
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/views/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML");
+		templateResolver.setCacheable(true);
+		return templateResolver;
+	}
+	
+	@Bean
+	public SpringTemplateEngine templateEngine(){
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		return templateEngine;
+	}
+	
+	@Bean
+	public ThymeleafViewResolver viewResolver(){
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
+		viewResolver.setOrder(1);
+		return viewResolver;
+	}
+
+}
