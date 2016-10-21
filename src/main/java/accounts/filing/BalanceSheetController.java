0dcb2a09f00f7eea9365d50422fda4b08bf8966c
flisowski <flisowski@companieshouse.gov.uk>
2016-10-21 16:10:05 +0100

    External configs working

--- src/main/java/accounts/filing/BalanceSheetController.java
+++ src/main/java/accounts/filing/BalanceSheetController.java
@@ -3,18 +3,15 @@ package accounts.filing;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class BalanceSheetController {
     
-	private String cdnUrl = "http://wswebdev2.orctel.internal:21502";
 
     @RequestMapping("/balanceSheet")
 	public String showBalanceSheetPage(Model model){
-		
-		model.addAttribute("cdnUrl", cdnUrl);
+
 		return "balanceSheet";
 		
 	}
