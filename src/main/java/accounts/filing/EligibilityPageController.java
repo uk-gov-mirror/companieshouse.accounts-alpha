893adbea56b72f6287ea60b3c0a6d95b6a81931d
flisowski <flisowski@companieshouse.gov.uk>
2016-10-25 11:50:16 +0100

    Add Joe's eligibility page in thymeleaf

---
+++ src/main/java/accounts/filing/EligibilityPageController.java
@@ -0,0 +1,17 @@
+package accounts.filing;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class EligibilityPageController {
+    
+	@RequestMapping(value="/eligibility", method = RequestMethod.GET)
+	public String showEligibilityPage(){
+		return "eligibility";
+	}
+ 
+
+}
