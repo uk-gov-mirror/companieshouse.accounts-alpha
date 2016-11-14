f15a6ad373f52a86095357d76d411c69c9703bd2
rryan <ruthkryan@gmail.com>
2016-11-14 13:00:33 +0000

    add custom validator methods

--- src/main/java/accounts/filing/models/AccountingPolicies.java
+++ src/main/java/accounts/filing/models/AccountingPolicies.java
@@ -1,10 +1,10 @@
 package accounts.filing.models;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.AssertTrue;
 
 public class AccountingPolicies {
     
-	@NotNull(message = "Error banana")
+	//@NotNull(message = "Error banana")
 	private String preparationStatement;
 	
 	private String otherPreparation;
@@ -15,11 +15,22 @@ public class AccountingPolicies {
 	public AccountingPolicies(String preparationStatement) {
 		this.preparationStatement = preparationStatement;
 	}
+	
+	@AssertTrue(message = "Please ensure you choose the statement that reflects the way your accounts were prepared.") 
+	public boolean isPreparationStatementSelected(){
+		return preparationStatement != null && !preparationStatement.isEmpty(); 
+	}
+	
+	@AssertTrue(message = "Please ensure you choose the statement that reflects the way your accounts were prepared.") 
+	public boolean isOtherStatementNotEmpty(){		
+		return (preparationStatement.isEmpty() || preparationStatement == null) && otherPreparation.isEmpty() ? false : true;
+	}
 
 	public void setPreparationStatement(String preparationStatement){
 		//System.out.println("Before validation" + preparationStatement);
-		this.preparationStatement = formatPreparationStatement(preparationStatement);
+		//this.preparationStatement = formatPreparationStatement(preparationStatement);
 		//System.out.println("After validation" + this.preparationStatement);
+		this.preparationStatement = preparationStatement;
 	}
 	
 	public void setOtherPreparation(String otherPreparation){
@@ -36,14 +47,14 @@ public class AccountingPolicies {
 	}
 	
 	
-	private String formatPreparationStatement(String preparationStatement) {
-		if(preparationStatement.isEmpty()){	
-			if(this.otherPreparation.isEmpty()){
-				return null;
-			}
-			return this.otherPreparation;	
-		}
-		return preparationStatement;
-	}
+//	private String formatPreparationStatement(String preparationStatement) {
+//		if(preparationStatement.isEmpty()){	
+//			if(this.otherPreparation.isEmpty()){
+//				return null;
+//			}
+//			return this.otherPreparation;	
+//		}
+//		return preparationStatement;
+//	}
 
 }
