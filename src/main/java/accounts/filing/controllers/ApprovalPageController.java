74901b6b40d2431342d2d690a73d5007404550b9
jbrewer94 <user@companieshouse.gov.uk>
2016-11-29 17:55:20 +0000

    Updates to approval page (validation & error messages)

--- src/main/java/accounts/filing/controllers/ApprovalPageController.java
+++ src/main/java/accounts/filing/controllers/ApprovalPageController.java
@@ -1,19 +1,32 @@
 package accounts.filing.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import accounts.filing.models.Approval;
 
 
 @Controller
 public class ApprovalPageController {
     
 	@RequestMapping(value="/approval", method = RequestMethod.GET)
-	public String showLandingPage(){
+	public String showApprovalPage(Model model){
+		model.addAttribute("approval", new Approval());
 		return "approval";
 	}
- 
 
+@RequestMapping(value="/approval", method = RequestMethod.POST)
+public String submitApprovalPage(@ModelAttribute @Valid Approval approval, BindingResult result){
+	if (result.hasErrors()){
+		return "approval";
+	} 
+	return "redirect:/summary-page";
 }
- 
+}
 	
--- src/main/java/accounts/filing/models/Approval.java
+++ src/main/java/accounts/filing/models/Approval.java
@@ -1,19 +1,21 @@
 package accounts.filing.models;
 
-import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class Approval {
-
+	
+	@Size(min=1, max=120, message="Please provide name details of accounts approver(s)")
+	@NotNull( message="Please provide name details of accounts approver(s)")
 	private String directorConfirmation;
-	
-	@AssertTrue(message = "Please provide name details of accounts approver(s)") 
-	   public boolean isValidDirectorSelection(){
-	       if ( directorConfirmation == null) {
-	           return false;
-	       }            
-	       return true; 
-	   }
-	
+	@NotNull( message="Please provide the date when the accounts were approved")
+	private Integer dayConfirmation;
+	@NotNull( message="Please provide the date when the accounts were approved")
+	private Integer monthConfirmation;
+	@NotNull( message="Please provide the date when the accounts were approved")
+	private Integer yearConfirmation;
+
+
 	public Approval() {
     }
 
@@ -24,3 +26,29 @@ public class Approval {
 	public void setDirectorConfirmation(String directorConfirmation) {
 		this.directorConfirmation = directorConfirmation;
 	}
+
+	public Integer getDayConfirmation() {
+		return dayConfirmation;
+	}
+
+	public void setDayConfirmation(Integer dayConfirmation) {
+		this.dayConfirmation = dayConfirmation;
+	}
+
+	public Integer getMonthConfirmation() {
+		return monthConfirmation;
+	}
+
+	public void setMonthConfirmation(Integer monthConfirmation) {
+		this.monthConfirmation = monthConfirmation;
+	}
+
+	public Integer getYearConfirmation() {
+		return yearConfirmation;
+	}
+
+	public void setYearConfirmation(Integer yearConfirmation) {
+		this.yearConfirmation = yearConfirmation;
+	}
+	
+}
