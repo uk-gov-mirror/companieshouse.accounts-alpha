49b47877c8b4d744468db1ff1dc349271196ae24
jbrewer94 <user@companieshouse.gov.uk>
2016-12-01 14:15:39 +0000

    Updates to approval screen

--- src/main/java/accounts/filing/models/Approval.java
+++ src/main/java/accounts/filing/models/Approval.java
@@ -1,8 +1,6 @@
 package accounts.filing.models;
 
 import javax.validation.constraints.AssertTrue;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
