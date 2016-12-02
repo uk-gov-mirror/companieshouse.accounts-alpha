3a598ffdf824ac91aee209b433ad03589fbd1e4e
jbrewer94 <user@companieshouse.gov.uk>
2016-12-02 12:15:38 +0000

    Summary page view and controller created

---
+++ src/main/java/accounts/filing/controllers/SummaryPageController.java
@@ -0,0 +1,17 @@
+package accounts.filing.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class SummaryPageController {
+    
+	@RequestMapping(value="/summary", method = RequestMethod.GET)
+	public String showLandingPage(){
+		return "summaryPage";
+	}
+ 
+
+}
