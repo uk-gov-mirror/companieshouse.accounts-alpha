a49841ab4b5ba4bd900c3e9b9fb5767b66b2f857
jbrewer94 <user@companieshouse.gov.uk>
2016-12-07 11:07:29 +0000

    Updates to approval page. All button functionality added.

--- src/main/java/accounts/filing/controllers/SummaryPageController.java
+++ src/main/java/accounts/filing/controllers/SummaryPageController.java
@@ -3,9 +3,11 @@ package accounts.filing.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 
 @Controller
+@SessionAttributes({"balanceSheet", "accountingPolicies", "tangibleAssetsNote"})
 public class SummaryPageController {
     
 	@RequestMapping(value="/summary", method = RequestMethod.GET)
